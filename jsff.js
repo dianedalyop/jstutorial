82278220c62f5597657572766354cc3c5b8a9d40
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2023-07-22 05:12:52 +0100

    new code
    more javascript lessoncodes

---
+++ jsff.js
@@ -0,0 +1,21 @@
+function exfile(){
+
+document.getElementById("demo2").innerHTML ="name";
+
+}
+
+function exfile2(){
+document.getElementById("demo3").innerHTML = 5 + 6 ;
+
+}
+
+function replace(){
+document.write(5 + 6);
+} 
+
+let x,y,z;
+x = 5;
+y = 6;
+z = x + y;
+
+document.getElementById("demo4").innerHTML = "The value of z is "+ z + ".";
